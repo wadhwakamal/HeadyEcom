178fc918cbdfde28f7b1d14d4dd449530bff79f1
Kamal Wadhwa <kamal.r.wadhwa@gmail.com>
2018-07-22 15:13:21 +0530

    Initial Commit

---
+++ HeadyECom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeadyECom
+//
+//  Created by Personal on 22/07/18.
+//  Copyright © 2018 Kamal Wadhwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
