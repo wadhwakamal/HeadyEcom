bf05a053d2c4dc6aefaa4c6492d696ee659d8587
Kamal Wadhwa <kamal.r.wadhwa@gmail.com>
2018-07-23 20:07:58 +0530

    Refactored code to display data in TableView

--- HeadyECom/CategoryViewController.swift
+++ HeadyECom/CategoryViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 import CoreData
 
+enum ContentType {
+    case category, product, none
+}
+
 class CategoryViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
@@ -17,9 +21,12 @@ class CategoryViewController: UIViewController {
     
     var categories = [Category]()
     var products = [Product]()
+    var contentType = ContentType.none
     
     lazy var categoryResults: NSFetchedResultsController<Category> = {
         let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        moc.automaticallyMergesChangesFromParent = true
+        
         let fetchRequest: NSFetchRequest<Category> = Category.fetchRequest()
         fetchRequest.sortDescriptors = [NSSortDescriptor(key: "id", ascending: true)]
 
@@ -29,15 +36,24 @@ class CategoryViewController: UIViewController {
             fetchRequest.predicate = NSPredicate(format: "parentID == nil")
         }
 
-        let FRC = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: nil, cacheName: nil)
-        FRC.delegate = self
-        return FRC
+        let resultsController = NSFetchedResultsController<Category>(fetchRequest: fetchRequest, managedObjectContext: moc, sectionNameKeyPath: nil, cacheName: nil)
+        resultsController.delegate = self
+        
+        return resultsController
     }()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         if parentCategory == nil {
             NetworkManager.fetchProducts { [weak self] in
+//                tableView.reloadData()
                 
             }
         }
@@ -55,31 +71,53 @@ class CategoryViewController: UIViewController {
                 self.products = products
             }
 
-            tableView.reloadData()
+//            tableView.reloadData()
         } catch {
             print("Unable to fetch Category Data \(error)")
         }
+//        NotificationCenter.default.addObserver(self, selector: #selector(CategoryViewController.reloadData), name: NSNotification.Name("NSManagedObjectContextDidSave"), object: nil)
+    }
+    
+    @objc func reloadData() {
+//        self.tableView.reloadData()
+    }
+    
+    @objc func didTapCart() {
+        print("CaRRT")
+    }
+    
+    func setupViews() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(named:"shopping-cart"), style: .plain, target: self, action: #selector(CategoryViewController.didTapCart))
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.setupViews()
         self.setupContent()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
 }
 
 extension CategoryViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let count = categoryResults.fetchedObjects?.count, count > 0 {
-            return count
+        if categories.count > 0 {
+            contentType = .category
+            return categories.count
         }
         
-        if let category = self.parentCategory, let count = category.products?.allObjects.count, count > 0 {
-            return count
+        if products.count > 0 {
+            contentType = .product
+            return products.count
         }
         
         return 0
@@ -87,7 +125,9 @@ extension CategoryViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "subCategory", for: indexPath)
-        if let categories = categoryResults.fetchedObjects, categories.isNotEmpty {
+        
+        switch contentType {
+        case .category:
             let category = categories[indexPath.row]
             cell.textLabel?.text = category.name
             cell.detailTextLabel?.text = ""
@@ -95,25 +135,45 @@ extension CategoryViewController: UITableViewDataSource, UITableViewDelegate {
             if let subCategory = category.subCategory {
                 cell.detailTextLabel?.text = (Array(subCategory) as! [Category]).flatMap { $0.name }.joined(separator: "-")
             }
-        } else if let product = self.parentCategory?.products?.allObjects[indexPath.row] as? Product {
+        case .product:
+            let product = products[indexPath.row]
             cell.textLabel?.text = product.name
-            cell.detailTextLabel?.text = "\(product.taxName ?? "") \(product.taxValue)"
+            cell.detailTextLabel?.text = nil
+            
+        default:
+            assert(false, "Content type is not handled properly. Please check the data in categories and products. Also make sure proper contentType is implemented")
         }
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if let categories = categoryResults.fetchedObjects, categories.isNotEmpty {
+        
+        switch contentType {
+        case .category:
             let category = categories[indexPath.row]
             segueToSubCategory(category: category)
+        case .product:
+            let product = products[indexPath.row]
+            segueToProduct(product: product)
+        default:
+            assert(false, "Content type is not handled properly. Please check the data in categories and products. Also make sure proper contentType is implemented")
         }
     }
     
     func segueToSubCategory(category: Category) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "CategoryController") as! CategoryViewController
+        let vc = storyboard.instantiateViewController(withIdentifier: "CategoryVC") as! CategoryViewController
         vc.parentCategory = category
         vc.title = category.name
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func segueToProduct(product: Product) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "ProductVC") as! ProductViewController
+        vc.product = product
+        vc.title = self.parentCategory?.name
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
         navigationController?.pushViewController(vc, animated: true)
     }
 }
